11a3a4f714edc9fab44767770d83d2129c0fc32e
dkhavich19 <94299534+dkhavich19@users.noreply.github.com>
2021-12-13 10:17:32 -0500

    Getting specific player stats

--- nhl_scraper.py
+++ nhl_scraper.py
@@ -8,18 +8,46 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-teams_url = 'https://statsapi.web.nhl.com/api/v1/teams/'
-
 class NHLScraper:
     all_players = dict()
     all_p_list = list()
+    players_id_name_dict = dict()
 
+    def get_player_stats(self, user_player_name):
 
-    def get_player_stats(self):
-        place_holder = 'Zelda'
-        return place_holder
+        for i, n in list(self.players_id_name_dict.items()):
+            if(n == user_player_name):
+                user_player_id = i
+                break
+
+        user_player_stats = self.all_players[(user_player_name, user_player_id)]
+        user_player_stats = Player(user_player_id, user_player_name)
+        player_position = user_player_stats.fetch_game_stats()
+
+        if(player_position == False):
+            print('No stats amassed for the current season.')
+        else:
+
+            print('Showing player statistics for', user_player_name, user_player_id)
+
+            if(player_position == 'G'):
+                print('Wins:', user_player_stats.wins)
+                print('Losses:', user_player_stats.losses)
+                print('gAA:', user_player_stats.gAA)
+                print('Shutouts', user_player_stats.shutouts)
+                print('Games:', user_player_stats.games)
+            else:
+                print('Points:', user_player_stats.points)
+                print('Goals:', user_player_stats.goals)
+                print('Assists:', user_player_stats.assists)
+                print('Shots:', user_player_stats.shots)
+                print('Games:', user_player_stats.games)
 
     def initialize_player_info(self):
+
+        print('\nLoading data, please wait...')
+
+        teams_url = 'https://statsapi.web.nhl.com/api/v1/teams/'
 
         team_data = urlopen(teams_url, context=ctx).read().decode()
         allteams_js = json.loads(team_data)
@@ -27,8 +55,6 @@ class NHLScraper:
         player_id_name_list = list()
         players_per_team_list = list()
 
-
-        players_id_name_dict = dict()
         list_id_name = list()
 
         for team_id in allteams_js['teams']:
@@ -37,16 +63,11 @@ class NHLScraper:
             roster_data = urlopen(roster_url, context=ctx).read().decode()
             team_roster_js = json.loads(roster_data)
 
-            # players_per_team = 0
             for player in team_roster_js['roster']:
-                players_id_name_dict[player['person']['id']] = player['person']['fullName']
-                # players_per_team += 1
+                self.players_id_name_dict[player['person']['id']] = player['person']['fullName']
                 p = Player(player['person']['id'], player['person']['fullName'])
                 self.all_players[(player['person']['fullName'], player['person']['id'])] = p
-                # if player['person']['id'] not in self.all_players:
-                #     print(player['person']['fullName'])
 
-        print(self.all_players)
 
             # players_per_team = 0
         #     for player in team_roster_js['roster']:
@@ -55,14 +76,7 @@ class NHLScraper:
         #         self.all_p_list.append(((player['person']['fullName'], player['person']['id']), p2))
         #     #     players_per_team += 1
         #     # players_per_team_list.append(players_per_team)
-        # print(len(players_id_name_dict))
-        # print(len(self.all_players))
-        # print('*******************************')
-        # print(len(player_id_name_list))
-        # print(len(self.all_p_list))
-        # print('*******************************')
-        # all_p_dict = dict(self.all_p_list)
-        # print(len(all_p_dict))
+
 
             # players_per_team2 = 0
             # for player in team_roster_js['roster']:
@@ -73,25 +87,64 @@ class NHLScraper:
             # players_per_team_list.append(players_per_team2)
 
 
-            #     print(self.all_players)
-            #     print(self.all_players[player['person']['fullName']].player_id)
-            #     print(self.all_players[player['person']['fullName']].player_name)
-            #     print(self.all_players[player['person']['fullName']].points)
-            #     break
-            # break
-
 class Player:
 
     player_id = int()
     player_name = str()
+
+    position = str()
+
+    # stats for skaters
     points = 0
     goals = 0
     assists = 0
     shots = 0
     games = 0
 
+    # stats for goalies
+    wins = 0
+    losses = 0
+    gAA = 0
+    shutouts = 0
+    games = 0
+
+
     def __init__(self, p_id, p_name):
         self.player_id = p_id
         self.player_name = p_name
         # print(self.player_id, 'is constructed')
         # print(self.player_name, 'is constructed')
+
+    def fetch_game_stats(self):
+
+        player_url = 'https://statsapi.web.nhl.com/api/v1/people/' + str(self.player_id)
+        player_data = urlopen(player_url, context=ctx).read().decode()
+        player_js = json.loads(player_data)
+
+        self.position = player_js['people'][0]['primaryPosition']['code']
+
+        stats_url_part1 = 'https://statsapi.web.nhl.com/api/v1/'
+        stats_url_part2 = '/stats?stats=statsSingleSeason&season=20212022'
+
+        stats_url = stats_url_part1 + 'people/' + str(self.player_id) + stats_url_part2
+        stats_data = urlopen(stats_url, context=ctx).read().decode()
+        stats_js = json.loads(stats_data)
+
+        if(stats_js['stats'][0]['splits'] == []):
+            return False
+        else:
+
+            if(self.position == 'G'):
+                self.wins = stats_js['stats'][0]['splits'][0]['stat']['wins']
+                self.losses = stats_js['stats'][0]['splits'][0]['stat']['losses']
+                self.gAA = stats_js['stats'][0]['splits'][0]['stat']['goalAgainstAverage']
+                self.shutouts = stats_js['stats'][0]['splits'][0]['stat']['shutouts']
+            else:
+                self.points = stats_js['stats'][0]['splits'][0]['stat']['points']
+                self.goals = stats_js['stats'][0]['splits'][0]['stat']['goals']
+                self.assists = stats_js['stats'][0]['splits'][0]['stat']['assists']
+                self.shots = stats_js['stats'][0]['splits'][0]['stat']['shots']
+
+            self.games = stats_js['stats'][0]['splits'][0]['stat']['games']
+
+            return self.position
--- run.py
+++ run.py
@@ -3,8 +3,6 @@ from nhl_scraper import *
 
 scraper = NHLScraper()
 
-
-
 print('Welcome to the NHL Scraper! Here are some options:')
 
 while True:
@@ -17,16 +15,27 @@ while True:
             from a certain country.''')
 
     user_option = input('\nEnter (1,2,3, or 4): ')
-    if (user_option != '1' or user_option != '2' or user_option != '3' or user_option != '4'):
+    if (user_option != '1' and user_option != '2' and user_option != '3' and user_option != '4'):
         print('\nPlease enter a number (1 through 4): ')
+
+    scraper.initialize_player_info()
+
+    # print(scraper.all_players)
+    # print(len(scraper.all_players))
+    #print(list(scraper.players_id_name_dict.items()))
+    # print(len(scraper.players_id_name_dict))
+
+
 
     if (user_option == '1'):
         print('\nYou have selected option 1: Retrieve current statistics for a particular player.')
-        user_player = input('Enter the name of the player: ')
-        print(scraper.get_player_stats())
-        print(NHLScraper.get_player_stats(scraper))
-        scraper.initialize_player_info()
-        # user_player = Player(45, 'ovechkin')
+        user_player_name = input('Enter the name of the player: ')
+
+        # user_player_name = 'Alex Ovechkin'
+        try:
+            player_id = scraper.get_player_stats(user_player_name)
+        except:
+            print('Not among currently active players.')
         break
 
     # elif (user_option == '2'):
